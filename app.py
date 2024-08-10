93cf8766142c69503802a424f37131a985efa1cb
valdemiro <valdemiro128@outlook.com>
2024-08-10 17:56:25 -0300

    Adição das funções model

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
 import sqlite3
-from crud import inserirProdutos
 
+from models.crud import verificarID
+from models.show import mostrarTabelaVenda, mostrarTabelaCliente, mostrarTabelaProduto
+from models.show import mostrarCliente, mostrarProduto, mostrarVenda
+from models.insercao import inserirClientes, inserirProdutos, inserirVendas
+from models.delete import delRegistroCliente, delRegistroProduto, delRegistroVenda
+from models.edit import updateCliente, updateProduto, updateVenda
 
 app = Flask(__name__)
 
@@ -11,20 +16,13 @@ app = Flask(__name__)
 #READ
 @app.route('/')
 def home():
-  banco = sqlite3 .connect('vendas.db')
-  cursor = banco.cursor()
-
-  cursor.execute('SELECT * FROM cliente')
-  cliente = cursor.fetchall()
-
-  cursor.execute('SELECT * FROM produto')
-  produto = cursor.fetchall()
-
-  cursor.execute('SELECT * FROM venda')
-  venda = cursor.fetchall()
-
-  banco.close()
-  return render_template('home.html', cliente = cliente, produto = produto, venda = venda)
+ 
+  #Chamada das funções dentro de models
+  cliente = mostrarTabelaCliente()
+  produto = mostrarTabelaProduto()
+  venda = mostrarTabelaVenda()
+  
+  return render_template('home.html', cliente = cliente, produto = produto, venda=venda)
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -48,14 +46,7 @@ def cadCliente(): #FUNÇÃO DE CADASTRO QUE IRÁ FUNCIONAR DENTRO DA FUNÇÃO AD
   cidade = request.form['cidadeCliente']
   tel = request.form['telCliente']
 
-  #Conexão com o DB
-  banco = sqlite3.connect('vendas.db')
-  cursor = banco.cursor()
-
-  #Execução da inserção de valores
-  cursor.execute('INSERT INTO cliente VALUES(?,?,?,?,?,?)',(None,nome, email, data, cidade, tel))
-  banco.commit()
-  banco.close()
+  inserirClientes(None,nome, data, cidade, tel, email)
 
   #Redirecionamento para função home() que irá renderizar a página principal
   return redirect(url_for('home'))
@@ -72,14 +63,7 @@ def cadProduto(): #Nome da função que será chamada no formulário
   categoria = request.form['categoria']
   qtd = request.form['quantidade']
 
-  #Conexão com o DB
-  #banco = sqlite3.connect('vendas.db')
-  #cursor = banco.cursor()
-  #Execução da inserção de valores
-  #cursor.execute('INSERT INTO produto VALUES (?,?,?,?,?,?)', (None,nomeProduto, preco, marca, categoria, qtd))
   inserirProdutos(None,nomeProduto, preco, marca, categoria, qtd)
-  #banco.commit()
-  #banco.close()
 
   #Redirecionamento para função home() que irá renderizar a página principal
   return redirect(url_for('home'))
@@ -89,21 +73,16 @@ def cadProduto(): #Nome da função que será chamada no formulário
 def cadVenda():#Nome da função que será chamada no formulário
 
   #Conexão com o DB
-  banco = sqlite3.connect('vendas.db')
-  cursor = banco.cursor()
 
   #Valores pegos dos inputs do formulário
   data = request.form['dataVenda']
   valor_total = request.form['valor']
   idCliente = int(request.form['idCliente'])
-  idProd = request.form['idProd']
+  idProd = int(request.form['idProd'])
   nomeFunc = request.form['nomeFunc']
 
-  #Lógica para verificar a existência das chaves ID de cliente e produto
-  if verificarID("idcliente", "cliente", int(idCliente)) and verificarID('idprod', 'produto', int(idProd)):
-    cursor.execute('INSERT INTO venda VALUES (?,?,?,?,?,?)',(None, data, valor_total, idCliente, idProd, nomeFunc))
-    banco.commit()
-    banco.close()
+  
+  inserirVendas(None, data, valor_total, idCliente, idProd, nomeFunc)
 
   #Redirecionamento para função home() que irá renderizar a página principal
   return redirect(url_for('home'))
@@ -116,15 +95,9 @@ def cadVenda():#Nome da função que será chamada no formulário
 @app.route('/delCliente/<int:id>', methods = ['GET']) #Rota com método GET para pegar o ID único do Cliente
 def delCliente(id): #Função com o parâmetro id que foi pego na rota
     
-    #Conexão com o DB
-    banco = sqlite3.connect('vendas.db')
-    cursor = banco.cursor()
 
     #Execução da exclusão de dados
-    cursor.execute(f'DELETE FROM cliente WHERE idcliente = {id}')
-    banco.commit()
-    banco.close()
-    flash('DADOS DELETADOS')
+    delRegistroCliente(id)
     
     #Redirecionamento para a página home
     return redirect(url_for('home'))
@@ -134,14 +107,9 @@ def delCliente(id): #Função com o parâmetro id que foi pego na rota
 @app.route('/delProduto/<int:id>', methods = ['GET']) #Rota com método GET para pegar o ID único do Produto
 def delProduto(id):#Função com o parâmetro id que foi pego na rota
 
-  #Conexão com o DB
-  banco = sqlite3.connect('vendas.db')
-  cursor = banco.cursor()
 
   #Execução da exclusão de dados
-  cursor.execute(f'DELETE FROM produto WHERE idprod = {id}')
-  banco.commit()
-  banco.close()
+  delRegistroProduto(id)
 
   #Redirecionamento para a página home
   return redirect(url_for('home'))
@@ -150,14 +118,8 @@ def delProduto(id):#Função com o parâmetro id que foi pego na rota
 @app.route('/delVenda/<int:id>', methods = ['GET']) #Rota com método GET para pegar o ID único da Venda
 def delVenda(id):#Função com o parâmetro id que foi pego na rota
 
-  #Conexão com o DB
-  banco = sqlite3.connect('vendas.db')
-  cursor = banco.cursor()
-
   #Execução da exclusão de dados
-  cursor.execute(f'DELETE FROM venda WHERE idvenda = {id}')
-  banco.commit()
-  banco.close()
+  delRegistroVenda(id)
 
   #Redirecionamento para a página home
   return redirect(url_for('home'))
@@ -173,14 +135,8 @@ def editarCliente(id):#parâmetro id pego na url da rota
   #Lógica para mostrar a linha proveniente da tabela cliente com o id do cliente
   if request.method == 'GET': 
       
-      #Conexão com o BD
-      banco = sqlite3.connect('vendas.db')
-      cursor = banco.cursor()
-
       #Execução para mostrar os dados
-      cursor.execute(f'SELECT * FROM cliente WHERE idCliente = {id}')
-      cliente = cursor.fetchone()
-      cursor.close()
+      cliente = mostrarCliente(id)
       return render_template('editarCliente.html', cliente = cliente)
   
   #Lógica para edição da linha do id 
@@ -191,14 +147,7 @@ def editarCliente(id):#parâmetro id pego na url da rota
     cidade = request.form['cidadeCliente']
     tel = request.form['telCliente']
 
-    #Conexão com o BD
-    banco = sqlite3.connect('vendas.db')
-    cursor = banco.cursor()
-
-    #Execução para atualizar os dados
-    cursor.execute('UPDATE cliente SET nome=?, email=?, data_nasc=?, cidade=?, telefone=? WHERE idCliente=?', (nome, email, data, cidade, tel, id))
-    banco.commit()
-    banco.close()
+    updateCliente(id,nome, email, data, cidade,tel)
 
     #Redirecionamento para a página
     return redirect(url_for('home'))
@@ -211,14 +160,8 @@ def editarProduto(id): #parâmetro id pego na url da rota
   #Lógica para mostrar a linha proveniente da tabela produto com o id do produto
   if request.method == 'GET': 
 
-    #Conexão com o BD
-    banco = sqlite3.connect('vendas.db')
-    cursor = banco.cursor()
-
     #Execução para mostrar os dados
-    cursor.execute(f'SELECT * FROM produto WHERE idprod={id}')
-    produto = cursor.fetchone()
-    cursor.close()
+    produto = mostrarProduto(id)
 
     return render_template('editarProduto.html', produto = produto)
   
@@ -230,13 +173,9 @@ def editarProduto(id): #parâmetro id pego na url da rota
     qtd = request.form['quantidade']
 
     #Conexão com o BD
-    banco = sqlite3.connect('vendas.db')
-    cursor = banco.cursor()
 
     #Execução para atualizar os dados
-    cursor.execute('UPDATE produto SET nome=?, preco=?, marca=?, categoria=?, qtd=? WHERE idprod = ?',(nomeProduto, preco, marca, categoria, qtd, id))
-    banco.commit()
-    banco.close()
+    updateProduto(id, nomeProduto, preco, marca, categoria, qtd)
     
     #Redirecionamento para a página
     return redirect(url_for('home'))
@@ -248,35 +187,24 @@ def editarVenda(id): #parâmetro id pego na url da rota
 
   #Lógica para mostrar a linha proveniente da tabela venda com o id da venda
   if request.method == 'GET':
-    banco = sqlite3.connect('vendas.db')
-    cursor = banco.cursor()
 
     #Execução para mostrar os dados
-    cursor.execute(f'SELECT * FROM venda WHERE idvenda={id}')
-    venda = cursor.fetchone()
-    cursor.close()
+    venda = mostrarVenda(id)
 
     return render_template('editarVenda.html', venda = venda)
   
   #Lógica para edição da linha do id 
   elif request.method == 'POST': 
 
-    banco = sqlite3.connect('vendas.db')
-    cursor = banco.cursor()
-
     data = request.form['dataVenda']
     valor_total = request.form['valor']
     idCliente = int(request.form['idCliente'])
-    idProd = request.form['idProd']
+    idProd = int(request.form['idProd'])
     nomeFunc = request.form['nomeFunc']
     
     #Lógica para verificar a existência nos ID do formulário
-    if verificarID("idcliente", "cliente", int(idCliente)) and verificarID('idprod', 'produto', int(idProd)):
-      cursor.execute('UPDATE venda SET data_venda=?, valor_total=?, idcliente=?, idprod=?, nomeFunc=? WHERE idvenda = ?',(data, valor_total, idCliente, idProd, nomeFunc, id))
-      banco.commit()
-      banco.close()
-    else:
-      flash('IDS INVÁLIDOS, TENTE NOVAMENTE')
+    if verificarID("cliente", "idcliente", int(idCliente)) and verificarID('produto', 'idprod', int(idProd)):
+      updateVenda(id, data, valor_total, idCliente, idProd, nomeFunc)
 
     #Redirecionamento para a página home
     return redirect(url_for('home'))
@@ -284,16 +212,17 @@ def editarVenda(id): #parâmetro id pego na url da rota
 
 
 #função de verificação
+'''
 def verificarID(coluna, tabela, id):
-    banco = sqlite3.connect('vendas.db')
-    cursor = banco.cursor()
+      banco = sqlite3.connect('vendas.db')
+      cursor = banco.cursor()
 
-    cursor.execute(f'SELECT {coluna} FROM {tabela}')
-    lista = cursor.fetchall()
-    for i in range (len(lista)):
-        if id == lista[i][0]:
-            return True 
-
+      cursor.execute(f'SELECT {coluna} FROM {tabela}')
+      lista = cursor.fetchall()
+      for i in range (len(lista)):
+          if id == lista[i][0]:
+              return True 
+'''
 
 if __name__ == "__main__":
   app.secret_key = 'admin123' 
